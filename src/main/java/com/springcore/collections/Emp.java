aad1a4d5bc9284920af6d44d9a3955b4244676a8
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-26 12:40:24 +0545

    Injecting Collection Types List , Set , Map

---
+++ src/main/java/com/springcore/collections/Emp.java
@@ -0,0 +1,57 @@
+package com.springcore.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Emp {
+private String name;
+private List<String> Phones;
+private Set<String> address;
+private Map<String,String> Courses;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public List<String> getPhones() {
+	return Phones;
+}
+public void setPhones(List<String> phones) {
+	Phones = phones;
+}
+public Set<String> getAddress() {
+	return address;
+}
+public void setAddress(Set<String> address) {
+	this.address = address;
+}
+public Map<String, String> getCourses() {
+	return Courses;
+}
+public void setCourses(Map<String, String> courses) {
+	Courses = courses;
+}
+public Emp(String name, List<String> phones, Set<String> address, Map<String, String> courses) {
+	super();
+	this.name = name;
+	Phones = phones;
+	this.address = address;
+	Courses = courses;
+}
+
+public Emp() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+@Override
+public String toString() {
+	return "Emp [name=" + name + ", Phones=" + Phones + ", address=" + address + ", Courses=" + Courses + "]";
+}
+
+
+
+
+
+}
---
+++ src/main/java/com/springcore/collections/Test.java
@@ -0,0 +1,14 @@
+package com.springcore.collections;
+import org.springframework.context.ApplicationContext;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context =new ClassPathXmlApplicationContext("com/springcore/collections/collectionsConfig.xml");
+        Emp emp1=(Emp) context.getBean("emp1");
+        System.out.println(emp1);
+		
+	}
+
+}
