6c257045c3d1db2d6087315e645f36ae4811e384
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-26 08:49:22 +0545

    add a config file to featch a object

--- src/main/java/com/springcore/springcore/App.java
+++ src/main/java/com/springcore/springcore/App.java
@@ -4,10 +4,22 @@ package com.springcore.springcore;
  * Hello world!
  *
  */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/**
+ * Hello world!
+ *
+ */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+     // create and configure beans
+        ApplicationContext context =new ClassPathXmlApplicationContext("config.xml");
+        Student student1=(Student) context.getBean("student1");
+        System.out.println(student1);
+        
     }
 }
+
