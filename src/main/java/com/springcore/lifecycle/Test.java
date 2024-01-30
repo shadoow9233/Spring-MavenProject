c080cc9ebc8b1ebb568e80dc90d45f010f9b24ee
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-30 08:30:58 +0545

    Implementing Lifecycle methods Using XML

---
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -0,0 +1,24 @@
+package com.springcore.lifecycle;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springcore.ref.A;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context =new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
+        Samosa s=(Samosa) context.getBean("s1");
+        System.out.println(s);
+        
+        /* to call a destory method we enable  a pre shutdown hook */
+        ((AbstractApplicationContext) context).registerShutdownHook();
+        
+
+		
+
+	}
+
+}
