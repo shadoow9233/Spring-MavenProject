6356246de4164e94ec94dabd9f758d16267748dd
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-01-30 10:37:06 +0545

    Implementing Bean LifeCyle using Annotations | @PostConstruct | @PreDestroy

--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -1,27 +1,31 @@
 package com.springcore.lifecycle;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.springcore.ref.A;
-
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ApplicationContext context =new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
-        Samosa s=(Samosa) context.getBean("s1");
-        System.out.println(s);
-        
-        /* to call a destory method we enable  a pre shutdown hook */
-        ((AbstractApplicationContext) context).registerShutdownHook();
-        
-        
-        System.out.println("*******************************************");
-        Pepsi p=(Pepsi) context.getBean("p1");
-        System.out.println(p);
-		
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
 
-	}
+        // Retrieving and printing Samosa bean
+        Samosa s = (Samosa) context.getBean("s1");
+        System.out.println(s);
+
+        // Registering a shutdown hook to call destroy method
+        ((AbstractApplicationContext) context).registerShutdownHook();
+
+        System.out.println("*******************************************");
+
+        // Retrieving and printing Pepsi bean
+        Pepsi p = (Pepsi) context.getBean("p1");
+        System.out.println(p);
+
+        // Retrieving and printing Example bean
+        Example e = (Example) context.getBean("e1");
+        System.out.println(e);
+
+    }
 
 }
