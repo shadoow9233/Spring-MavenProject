97ac17e5bd3cd3fb54f97cd1988b3d216fa52054
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-02-02 05:59:49 +0545

    Autowiring using XML by name

---
+++ src/main/java/com/springcore/auto/wired/Address.java
@@ -0,0 +1,35 @@
+package com.springcore.auto.wired;
+
+public class Address {
+	private String Street;
+	private String city;
+	public String getStreet() {
+		return Street;
+	}
+	public void setStreet(String street) {
+		Street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+//	@Override
+//	public String toString() {
+//		// TODO Auto-generated method stub
+//		return super.toString();
+//	}
+//	
+	@Override
+	public String toString() {
+		return "Address [Street=" + Street + ", city=" + city + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/springcore/auto/wired/Emp.java
@@ -0,0 +1,42 @@
+package com.springcore.auto.wired;
+
+public class Emp {
+
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Emp(Address address) {
+		super();
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+
+
+
+//	@Override
+//	public String toString() {
+//		// TODO Auto-generated method stub
+//		return super.toString();
+//	}
+
+	
+	
+	
+	
+}
