7afd2bc6319510be473e2d9b363ab071014999af
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-02-04 09:09:00 +0545

    @Autowired Annotation for Autowiring

---
+++ src/main/java/com/springcore/auto/wired/annotation/Address.java
@@ -0,0 +1,36 @@
+package com.springcore.auto.wired.annotation;
+
+public class Address {
+	private String Street;
+	private String city;
+	public String getStreet() {
+		return Street;
+	}
+	public void setStreet(String street) {
+		Street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+//	@Override
+//	public String toString() {
+//		// TODO Auto-generated method stub
+//		return super.toString();
+//	}
+//	
+	@Override
+	public String toString() {
+		return "Address [Street=" + Street + ", city=" + city + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/springcore/auto/wired/annotation/Emp.java
@@ -0,0 +1,51 @@
+package com.springcore.auto.wired.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Emp {
+	//@Autowired
+
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+	
+	//auto wired used by setter value
+	//@Autowired
+	public void setAddress(Address address) {
+		System.out.println("Setting value");
+		this.address = address;
+		
+	}
+	
+	public Emp() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	//auto wired used by constructor annotations
+	@Autowired
+	public Emp(Address address) {
+		super();
+		this.address = address;
+		System.out.println("Inside constructor");
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [address=" + address + "]";
+	}
+
+
+
+//	@Override
+//	public String toString() {
+//		// TODO Auto-generated method stub
+//		return super.toString();
+//	}
+
+	
+	
+	
+	
+}
