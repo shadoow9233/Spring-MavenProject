f69918bbe095a13070c60cfcfafe4c9bde2da33d
Bibek Kunwar <kunwarbibek9233@gmail.com>
2024-02-14 07:46:15 +0545

    Spring Expression Language | SpEL

---
+++ src/main/java/com/springcore/spel/Test.java
@@ -0,0 +1,19 @@
+package com.springcore.spel;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/spel/spelconfig.xml");
+        Demo d1 = context.getBean("demo", Demo.class);
+        System.out.println(d1);
+
+        SpelExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("22+25");
+        System.out.println(expression.getValue());
+    }
+}
